17f626eddf3e71595e77d5e8defadd5aac730f15
Sci Nguyen <ngocsonguyen137@gmail.com>
2025-09-12 21:12:24 +0700

    tôi xin lỗi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,6 @@ function App() {
 
   return (
     <>
-      <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind v4 🚀
-      </h1>
-    </div>
     </>
   )
 }
