db624fb82a5bd4b52e6eedd73168962498ed47c8
HONGHA <cavoi2110@gmail.com>
2025-09-12 21:17:19 +0100

    keke

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-
+import Dangnhap from "./pages/Dangnhap"
+import Dangky from "./pages/Dangky"
 
 
 function App() {
@@ -6,9 +7,10 @@ function App() {
 
   return (
     <>
-      
+      <Dangnhap />
+      <Dangky />
     </>
-  )
+  );
 }
 
 export default App
