a97d163e84597d59af6e87f9f81c1a058d080901
Sci Nguyen <ngocsonguyen137@gmail.com>
2025-10-11 00:05:45 +0700

    feat: dựng môi trường cho database MySQL và Redis trong backend

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,12 +1,14 @@
 
 # ===== USER & ROLE =====
-from .user import User, UserRole, Address, Role
+from .user import User
+# from .user import UserRole, Address, Role
 
 # ===== PRODUCT & CATEGORY =====
-from .product import Product, Category, Brand, ProductImage, ProductSpecification
+from .product import Product , Category
+# from .product import Category, Brand, ProductImage, ProductSpecification
 
 # ===== CART & ORDER =====
-from .cart_order import Cart, CartItem, Order, OrderItem, Payment, Shipping
+# from .cart_order import Cart, CartItem, Order, OrderItem, Payment, Shipping
 
 # ===== COUPON, REVIEW, WISHLIST =====
-from .extras import Coupon, Review,Wishlist, WishlistItem 
+# from .extras import Coupon, Review,Wishlist, WishlistItem 
